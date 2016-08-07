f3041a2cab3d597b8b3a016a94e20a3f92f2b205
[TomsDay] <[mliangshuai@126.com]>
2016-08-07 19:52:25 +0800

    2016 08 07 update bean

--- app/src/main/java/com/suken/bridgedetection/bean/GeteMaterialBean.java
+++ app/src/main/java/com/suken/bridgedetection/bean/GeteMaterialBean.java
@@ -12,109 +12,121 @@ import java.io.Serializable;
 public class GeteMaterialBean implements Serializable {
     @DatabaseField(generatedId = true) //主键自增加
     private int ids;
-    /**记录创建人ID */
-    @DatabaseField
-    private String createBy;
-    /**记录创建时间 */
-    @DatabaseField
-    private String createtime;
-    /**记录创建人 */
-    @DatabaseField
-    private String creator;
-    /**记录是否有效 0 有效  1无效 */
-    @DatabaseField
-    private String flag;
-    /**归属组织机构，暂不用 */
-    @DatabaseField
-    private String orgid;
-    /**记录最后更新人ID */
-    @DatabaseField
-    private String updateBy;
-    /**记录最后更新时间 */
-    @DatabaseField
-    private String updatetime;
-    /**记录最后更新人 */
-    @DatabaseField
-    private String updator;
-    /**记录更新版本号 */
-    @DatabaseField
-    private String versionno;
-    /**管养单位ID */
-    @DatabaseField
-    private String gydwId;
-    /**管养单位名称 */
-    @DatabaseField
-    private String gydwName;
-    /**记录ID */
-    @DatabaseField
-    private String id;
-    /**一级分类 */
-    @DatabaseField
-    private String yjml;
-    /**二级分类 */
-    @DatabaseField
-    private String ejml;
-    /**材料名称 */
     @DatabaseField
     private String clmc;
-    /**损坏类型 */
     @DatabaseField
-    private String shlx;
-    /**单位 */
+    private String clno;
+    @DatabaseField
+    private String createBy;
+    @DatabaseField
+    private String createtime;
+    @DatabaseField
+    private String creator;
     @DatabaseField
     private String dw;
-    /**现场描述 */
     @DatabaseField
-    private String xcms;
+    private String flag;
+    @DatabaseField
+    private String gg;
+    @DatabaseField
+    private String gydwId;
+    @DatabaseField
+    private String gydwName;
+    @DatabaseField
+    private String id;
+    @DatabaseField
+    private String orgid    ;
+    @DatabaseField
+    private String updateBy;
+    @DatabaseField
+    private String updatetime;
+    @DatabaseField
+    private String updator;
+    @DatabaseField
+    private String versionno;
+    @DatabaseField
+    private String xh;
+
+
+
+
+//    /**记录创建人ID */
+//    @DatabaseField
+//    private String createBy;
+//    /**记录创建时间 */
+//    @DatabaseField
+//    private String createtime;
+//    /**记录创建人 */
+//    @DatabaseField
+//    private String creator;
+//    /**记录是否有效 0 有效  1无效 */
+//    @DatabaseField
+//    private String flag;
+//    /**归属组织机构，暂不用 */
+//    @DatabaseField
+//    private String orgid;
+//    /**记录最后更新人ID */
+//    @DatabaseField
+//    private String updateBy;
+//    /**记录最后更新时间 */
+//    @DatabaseField
+//    private String updatetime;
+//    /**记录最后更新人 */
+//    @DatabaseField
+//    private String updator;
+//    /**记录更新版本号 */
+//    @DatabaseField
+//    private String versionno;
+//    /**管养单位ID */
+//    @DatabaseField
+//    private String gydwId;
+//    /**管养单位名称 */
+//    @DatabaseField
+//    private String gydwName;
+//    /**记录ID */
+//    @DatabaseField
+//    private String id;
+//    /**一级分类 */
+//    @DatabaseField
+//    private String yjml;
+//    /**二级分类 */
+//    @DatabaseField
+//    private String ejml;
+//    /**材料名称 */
+//    @DatabaseField
+//    private String clmc;
+//    /**损坏类型 */
+//    @DatabaseField
+//    private String shlx;
+//    /**单位 */
+//    @DatabaseField
+//    private String dw;
+//    /**现场描述 */
+//    @DatabaseField
+//    private String xcms;
 
     public GeteMaterialBean() {
     }
 
-    public GeteMaterialBean(int ids, String createBy, String createtime, String creator, String flag, String orgid, String updateBy, String updatetime, String updator, String versionno, String gydwId, String gydwName, String id, String yjml, String ejml, String clmc, String shlx, String dw, String xcms) {
+    public GeteMaterialBean(int ids, String clmc, String clno, String createBy, String createtime, String creator, String dw, String flag, String gg, String gydwId, String gydwName, String id, String orgid, String updateBy, String updatetime, String updator, String versionno, String xh) {
         this.ids = ids;
+        this.clmc = clmc;
+        this.clno = clno;
         this.createBy = createBy;
         this.createtime = createtime;
         this.creator = creator;
+        this.dw = dw;
         this.flag = flag;
+        this.gg = gg;
+        this.gydwId = gydwId;
+        this.gydwName = gydwName;
+        this.id = id;
         this.orgid = orgid;
         this.updateBy = updateBy;
         this.updatetime = updatetime;
         this.updator = updator;
         this.versionno = versionno;
-        this.gydwId = gydwId;
-        this.gydwName = gydwName;
-        this.id = id;
-        this.yjml = yjml;
-        this.ejml = ejml;
-        this.clmc = clmc;
-        this.shlx = shlx;
-        this.dw = dw;
-        this.xcms = xcms;
-    }
-
-    @Override
-    public String toString() {
-        return "GeteMaterialBean{" +
-                "ids=" + ids +
-                ", createBy='" + createBy + '\'' +
-                ", createtime='" + createtime + '\'' +
-                ", creator='" + creator + '\'' +
-                ", flag='" + flag + '\'' +
-                ", orgid='" + orgid + '\'' +
-                ", updateBy='" + updateBy + '\'' +
-                ", updatetime='" + updatetime + '\'' +
-                ", updator='" + updator + '\'' +
-                ", versionno='" + versionno + '\'' +
-                ", gydwId='" + gydwId + '\'' +
-                ", gydwName='" + gydwName + '\'' +
-                ", id='" + id + '\'' +
-                ", yjml='" + yjml + '\'' +
-                ", ejml='" + ejml + '\'' +
-                ", clmc='" + clmc + '\'' +
-                ", shlx='" + shlx + '\'' +
-                ", dw='" + dw + '\'' +
-                ", xcms='" + xcms + '\'' +
-                '}';
+        this.xh = xh;
     }
 
     public int getIds() {
@@ -123,6 +135,22 @@ public class GeteMaterialBean implements Serializable {
 
     public void setIds(int ids) {
         this.ids = ids;
+    }
+
+    public String getClmc() {
+        return clmc;
+    }
+
+    public void setClmc(String clmc) {
+        this.clmc = clmc;
+    }
+
+    public String getClno() {
+        return clno;
+    }
+
+    public void setClno(String clno) {
+        this.clno = clno;
     }
 
     public String getCreateBy() {
@@ -149,12 +177,52 @@ public class GeteMaterialBean implements Serializable {
         this.creator = creator;
     }
 
+    public String getDw() {
+        return dw;
+    }
+
+    public void setDw(String dw) {
+        this.dw = dw;
+    }
+
     public String getFlag() {
         return flag;
     }
 
     public void setFlag(String flag) {
         this.flag = flag;
+    }
+
+    public String getGg() {
+        return gg;
+    }
+
+    public void setGg(String gg) {
+        this.gg = gg;
+    }
+
+    public String getGydwId() {
+        return gydwId;
+    }
+
+    public void setGydwId(String gydwId) {
+        this.gydwId = gydwId;
+    }
+
+    public String getGydwName() {
+        return gydwName;
+    }
+
+    public void setGydwName(String gydwName) {
+        this.gydwName = gydwName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getOrgid() {
@@ -197,75 +265,35 @@ public class GeteMaterialBean implements Serializable {
         this.versionno = versionno;
     }
 
-    public String getGydwId() {
-        return gydwId;
+    public String getXh() {
+        return xh;
     }
 
-    public void setGydwId(String gydwId) {
-        this.gydwId = gydwId;
+    public void setXh(String xh) {
+        this.xh = xh;
     }
 
-    public String getGydwName() {
-        return gydwName;
-    }
-
-    public void setGydwName(String gydwName) {
-        this.gydwName = gydwName;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getYjml() {
-        return yjml;
-    }
-
-    public void setYjml(String yjml) {
-        this.yjml = yjml;
-    }
-
-    public String getEjml() {
-        return ejml;
-    }
-
-    public void setEjml(String ejml) {
-        this.ejml = ejml;
-    }
-
-    public String getClmc() {
-        return clmc;
-    }
-
-    public void setClmc(String clmc) {
-        this.clmc = clmc;
-    }
-
-    public String getShlx() {
-        return shlx;
-    }
-
-    public void setShlx(String shlx) {
-        this.shlx = shlx;
-    }
-
-    public String getDw() {
-        return dw;
-    }
-
-    public void setDw(String dw) {
-        this.dw = dw;
-    }
-
-    public String getXcms() {
-        return xcms;
-    }
-
-    public void setXcms(String xcms) {
-        this.xcms = xcms;
+    @Override
+    public String toString() {
+        return "GeteMaterialBean{" +
+                "ids=" + ids +
+                ", clmc='" + clmc + '\'' +
+                ", clno='" + clno + '\'' +
+                ", createBy='" + createBy + '\'' +
+                ", createtime='" + createtime + '\'' +
+                ", creator='" + creator + '\'' +
+                ", dw='" + dw + '\'' +
+                ", flag='" + flag + '\'' +
+                ", gg='" + gg + '\'' +
+                ", gydwId='" + gydwId + '\'' +
+                ", gydwName='" + gydwName + '\'' +
+                ", id='" + id + '\'' +
+                ", orgid='" + orgid + '\'' +
+                ", updateBy='" + updateBy + '\'' +
+                ", updatetime='" + updatetime + '\'' +
+                ", updator='" + updator + '\'' +
+                ", versionno='" + versionno + '\'' +
+                ", xh='" + xh + '\'' +
+                '}';
     }
 }
